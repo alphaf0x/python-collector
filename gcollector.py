6f82ba716fd42cc35c52b0201a51893e4723c382
SageDark <Control.yay@gmail.com>
2018-10-12 18:11:15 +0200

    list all filenames in the directory and file number

--- gcollector.py
+++ gcollector.py
@@ -1,3 +1,23 @@
-#this program was made to automate the process of cleaning up
-#your downloads folder
+#this program was made to automate the process of cleaning up your downloads folder
+#Written in Python 3.7
 #SageDark
+
+import os
+import glob
+
+path = 'C:\\Users\\USER\\Downloads'
+
+#get the number of files in the directory
+onlyfiles = next(os.walk(path))[2]
+print (len(onlyfiles))
+
+#get a list with all filenames
+filenames = glob.glob(path + "\\*")
+
+def get_date(filename):
+    lastmodified = os.path.getmtime(path + '\\' + filename)
+    return lastmodified
+
+
+for files in range(len(onlyfiles)):
+    print (filenames[files])
