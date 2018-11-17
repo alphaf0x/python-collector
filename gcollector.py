719dbcf3b99480db4e7f568498401d50ec9d8ede
Wiktor Wielento <Control.yay@gmail.com>
2018-11-17 15:26:10 +0000

    Add working time difference TODO: get_date function

--- gcollector.py
+++ gcollector.py
@@ -5,8 +5,11 @@
 
 import os
 import glob
+import os.path
+import time
 
 path = 'C:\\Users\\contr\\Downloads'
+# timestamp equivalent to 31 days
 timediff = 2592000
 
 # get the number of files in the directory
@@ -15,10 +18,18 @@ print(len(onlyfiles))
 
 # get a list with all filenames
 filenames = glob.glob(path + "\\*")
+modifiedFiles = 0
 for files in range(len(onlyfiles)):
-    filenames[files] = filenames[files].strip(path)
+        if (time.time()) - os.path.getmtime(filenames[files]) > timediff:
+            modifiedFiles += 1
+            print(filenames[files])
+print('Out of ' + str(len(onlyfiles)) + ' files, ' + str(modifiedFiles) + ' are older than 31 days.')
 
 
 def get_date(filename):
     lastmodified = os.path.getmtime(path + '\\' + filename)
+    print(lastmodified)
     return lastmodified
+
+
+# get_date(filenames)
